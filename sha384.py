9445fdece6bf41f2d18ccae695ba7f5da2c9ac7c
jflashman <joshua@flashman.org>
2021-11-07 18:59:04 -0500

    Renamed to sha384, fixing execution as well

--- sha384.py
+++ sha384.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-def sha284(input):
+def sha384(input):
     encoded_input = input.encode('utf-8')
-    hashed_input = hashlib.sha284(encoded_input).hexdigest()
+    hashed_input = hashlib.sha384(encoded_input).hexdigest()
     return hashed_input
