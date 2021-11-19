0a476aa252fbbc4465e6b9c2c937a4ea774000ea
eduardo <eduardoneirasacoto1337@gmail.com>
2021-11-19 13:31:00 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-#Missing: Decryptions for OFB, CTR modes. Encryptions/Decryptions for GCM / ECB Modes
+#Missing: Decryptions for OFB, CTR modes. Encryptions/Decryptions for ECB Modes
 
 import hashlib
 from Cryptodome import Random
 from Cryptodome.Cipher import AES
+from Cryptodome.Protocol.KDF import scrypt
 from base64 import b64encode, b64decode
 from secrets import token_bytes
 
@@ -40,6 +41,20 @@ class AESCipher(object):
         encrypted_text = cipher.encrypt(plain_text.encode())
         return b64encode(encrypted_text).decode("utf-8")
 
+    def encryptGCM(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        cipher = AES.new(self.key, AES.MODE_GCM)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(encrypted_text).decode("utf-8")
+
+    def encryptECB(self, plain_text):
+        plain_text = self.__pad(plain_text)
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        encrypted_text = cipher.encrypt(plain_text.encode())
+        return b64encode(encrypted_text).decode("utf-8")
+
+
+
 
     def decryptCBC(self, encrypted_text):
         encrypted_text = b64decode(encrypted_text)
@@ -52,9 +67,34 @@ class AESCipher(object):
         encrypted_text = b64decode(encrypted_text)
         iv = encrypted_text[:self.block_size]
         cipher = AES.new(self.key, AES.MODE_CFB, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self.__unpad(plain_text)
+
+    def decryptOFB(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_OFB, iv)
         plain_text = cipher.decrypt(encrypted_text[self.block_size:])
         return self.__unpad(plain_text)
 
+    def decryptCTR(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        cipher = AES.new(self.key, AES.MODE_CTR)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:])
+        return self.__unpad(plain_text)
+
+    def decryptGCM(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = AES.new(self.key, AES.MODE_GCM, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:])
+        return self.__unpad(plain_text)
+
+    def decryptECB(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        cipher = AES.new(self.key, AES.MODE_ECB)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:])
+        return self.__unpad(plain_text)
 
     def __pad(self, plain_text):
         number_of_bytes_to_pad = self.block_size - len(plain_text) % self.block_size
@@ -78,10 +118,15 @@ print('Encrypted value CBC: ', AESCipher(key).encryptCBC(msg))
 print('Encrypted value CFB: ', AESCipher(key).encryptCFB(msg))
 print('Encrypted value OFB: ', AESCipher(key).encryptOFB(msg))
 print('Encrypted value CTR: ', AESCipher(key).encryptCTR(msg))
-
-
-
+print('Encrypted value GCM: ', AESCipher(key).encryptGCM(msg))
+print('Encrypted value ECB: ', AESCipher(key).encryptECB(msg))
 
 
 print('Decrypted value CBC: ', AESCipher(key).decryptCBC(p))
 print('Decrypted value CFB: ', AESCipher(key).decryptCFB(p))
+print('Decrypted value OFB: ', AESCipher(key).decryptOFB(p))
+print('Decrypted value CTR: ', AESCipher(key).decryptCTR(p))
+print('Decrypted value GCM: ', AESCipher(key).decryptGCM(p))
+print('Decrypted value ECB: ', AESCipher(key).decryptECB(p))
+
+
